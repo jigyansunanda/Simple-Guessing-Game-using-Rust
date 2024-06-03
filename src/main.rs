ee40d7426b7e2f15b576c91a2f3f0ef7555b1c3b
Jigyansu Nanda <nandajigyansu@gmail.com>
2024-06-03 11:50:08 +0530

    logic implemented

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,41 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("GUESS THE NUMBER !!!\n");
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guessed_number = String::new();
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guessed_number)
-        .expect("Failed to read input");
-    println!("You guessed: {guessed_number}");
+        // create a variable to store user guess input
+        let mut guessed_number = String::new();
+
+        // read the user input
+        io::stdin()
+            .read_line(&mut guessed_number)
+            .expect("Failed to read input");
+
+        // print user input
+        println!("You guessed: {guessed_number}");
+
+        // convert user input into string,
+        // in case of invalid conversions,
+        // keep asking for numeric string input
+        let guessed_number: u32 = match guessed_number.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        // continue asking for input unitl it's a match.
+        match guessed_number.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
